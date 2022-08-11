58b8ee536d566b1a449c944d389457ebfdead33b
AWOS <uws0331@gmail.com>
2022-08-11 20:11:41 +0900

    싱글파일 컴포넌트 체계에서 컴포넌트 등록

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<!-- 컴포넌트 태그는 기존 html 태그와 혼동할 수 있기 때문에
+가능하면 두단어 이상으로 조합해서 명명
+-->
+<template>
+  <header>
+    <h1>Header</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
