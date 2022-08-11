df817893aaf5af92d2b6421a2fbf933cf8fd4a4a
AWOS <uws0331@gmail.com>
2022-08-12 00:31:14 +0900

    axios를 이용한 데이터 전송 및 form 구현

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -2,7 +2,8 @@
   <div>
     <!-- <form action=""> -->
     <!-- form의 기본동작이 새로고침 -->
-    <form v-on:submit="submitForm">
+    <!-- 34번째줄과 같은 기능 -->
+    <form v-on:submit.prevent="submitForm">
       <div>
         <label for="username">id: </label>
         <input id="username" type="text" v-model="username">
@@ -17,6 +18,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   // 컴포넌트 단위로 개발시
   // 컴포넌트 간의 데이터가 공유되지 않게
@@ -28,10 +31,23 @@ export default {
     }
   },
   methods: {
-    submitForm: function (event) {
+    submitForm: function () {
       // 새로고침을 막는 방법
-      event.preventDefault();
+      // event.preventDefault();
       console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password,
+      }
+      // http 프로토콜을 이용한 통신
+      axios.post(url, data)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
   }
 }
